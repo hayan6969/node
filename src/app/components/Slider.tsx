d3f7fc8d60ec19f4e54b36a948fea30fae28f121
Hayan Khan <hayankhanmehsood@gmail.com>
2025-02-23 19:05:09 +0500

    .

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -3,43 +3,67 @@ import { Marquee } from "@/components/magicui/marquee";
 import Image from "next/image";
 
 const reviews = [
-  {title: "VIDEOS",img: "https://images.unsplash.com/photo-1738940251292-49709608c8aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D",id:1},
-  {title: "LIVECAM",img: "https://images.unsplash.com/photo-1739382121077-7a20fed13566?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI2fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D",id:2},
-  {title: "GAMES",img: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z2FtZXxlbnwwfHwwfHx8MA%3D%3D",id:3},
-  {title: "VIDEOS",img: "https://images.unsplash.com/photo-1738940251292-49709608c8aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D",id:1},
-  {title: "LIVECAM",img: "https://images.unsplash.com/photo-1739382121077-7a20fed13566?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI2fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D",id:2},
-  {title: "GAMES",img: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z2FtZXxlbnwwfHwwfHx8MA%3D%3D",id:3},
-
+  {
+    title: "VIDEOS",
+    img: "https://images.unsplash.com/photo-1738940251292-49709608c8aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
+    id: 1,
+  },
+  {
+    title: "LIVECAM",
+    img: "https://images.unsplash.com/photo-1739382121077-7a20fed13566?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
+    id: 2,
+  },
+  {
+    title: "GAMES",
+    img: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
+    id: 3,
+  },
+  {
+    title: "VIDEOS",
+    img: "https://images.unsplash.com/photo-1738940251292-49709608c8aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
+    id: 1,
+  },
+  {
+    title: "LIVECAM",
+    img: "https://images.unsplash.com/photo-1739382121077-7a20fed13566?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
+    id: 2,
+  },
+  {
+    title: "GAMES",
+    img: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
+    id: 3,
+  },
 ];
 
 const firstRow = reviews.slice(0, reviews.length / 2);
 const secondRow = reviews.slice(reviews.length / 2);
 
-const ReviewCard = ({
-  img,
-  name,
-  username,
-  title,
-}: {
+type ReviewCardProps = {
   img: string;
-  name: string;
-  username: string;
   title: string;
-}) => {
+  name?: string;
+  username?: string;
+};
+
+const ReviewCard = ({ img, title, name, username }: ReviewCardProps) => {
   return (
     <figure
       className={cn(
         "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-        // light styles
         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
       )}
     >
-     <div className={`w-64 h-20 flex justify-center items-center text-5xl unbordered-white text-opacity-70 relative overflow-hidden`}>
-        <img src={img} alt="card_image" className="absolute w-full h-full top-0 left-0"></img>
-        <span className="absolute w-full h-full left-0 top-0 flex justify-center items-center text-center">{title}</span>
-        </div>
+      <div className="w-64 h-20 flex justify-center items-center text-5xl unbordered-white text-opacity-70 relative overflow-hidden">
+        <img
+          src={img}
+          alt="card_image"
+          className="absolute w-full h-full top-0 left-0"
+        />
+        <span className="absolute w-full h-full left-0 top-0 flex justify-center items-center text-center">
+          {title}
+        </span>
+      </div>
     </figure>
   );
 };
